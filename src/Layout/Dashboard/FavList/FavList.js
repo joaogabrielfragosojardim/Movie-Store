7b4d8e6a9fd9e3efd5e92f8689eb6032f7b05bba
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2021-10-21 20:05:43 -0300

    on key up mobile

---
+++ src/Layout/Dashboard/FavList/FavList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../FavList/FavList.css"
+
+function FavList() {
+    return (
+        <div id="fav-list">
+            
+        </div>
+    )
+}
+
+export default FavList
