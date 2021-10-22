13a27aefed1a6b84695b1a85c22851cbe72bb0fe
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2021-10-21 21:57:53 -0300

    animation fav

--- src/Assets/FavIcon.js
+++ src/Assets/FavIcon.js
@@ -3,11 +3,31 @@ import React from "react";
 function FavIcon(props) {
   const width = props.width;
 
+  function showFavCard(c) {
+    const icon = c.target;
+    const containerFilms = document.getElementById("container-select-movies");
+    const containerFavList = document.getElementById("container-fav-list");
+    const body = document.getElementsByTagName("body")[0];
+
+    if (icon.classList.contains("expanded")) {
+      containerFilms.style.width = "100vw";
+      containerFavList.style.display = "none";
+      icon.classList.remove("expanded");
+      body.classList.remove("overflow-y");
+    } else {
+      icon.classList.add("expanded");
+      body.classList.add("overflow-y");
+      containerFilms.style.width = "calc(100vw - 450px)";
+      containerFavList.style.display = "inline";
+    }
+  }
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      style={{ fill: "currentcolor", width: width }}
+      style={{ fill: "currentcolor", width: width, cursor: "pointer" }}
+      onClick={showFavCard}
     >
       <path d="M12 4.419c-2.826-5.695-11.999-4.064-11.999 3.27 0 7.27 9.903 10.938 11.999 15.311 2.096-4.373 12-8.041 12-15.311 0-7.327-9.17-8.972-12-3.27z" />
     </svg>
--- src/Layout/Dashboard/Dashboard.js
+++ src/Layout/Dashboard/Dashboard.js
@@ -2,16 +2,17 @@ import React from "react";
 import MovieList from "./MovieList/MovieList";
 import Select from "./Select/Select";
 import FavList from "./FavList/FavList";
+import "../Dashboard/Dashboard.css";
 
 function Dashboard() {
   return (
-    <div>
-      <div>
-        <FavList />
-      </div>
-      <div>
+    <div id="container-dashboard">
+      <div id="container-select-movies" className="container-animation">
         <Select />
         <MovieList />
+      </div>
+      <div id="container-fav-list" className="container-animation">
+        <FavList />
       </div>
     </div>
   );
